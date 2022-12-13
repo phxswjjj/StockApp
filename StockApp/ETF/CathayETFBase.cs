f103bf0440c4e58ee238d887486ddedcea9985e4
phxswjjj <phxswjjj@gmail.com>
2022-12-13 22:47:57 +0800

    國泰ETF

---
+++ StockApp/ETF/CathayETFBase.cs
@@ -0,0 +1,56 @@
+﻿using AngleSharp.Dom;
+using AngleSharp;
+using StockApp.Group;
+using StockApp.Web;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.ETF
+{
+    abstract class CathayETFBase
+    {
+        public abstract string ComCode { get; }
+        protected abstract string SiteCode { get; }
+
+        internal CustomGroup GetAll()
+        {
+            var offseted = Utility.TWSEDate.Today;
+            var jsonFilePath = Path.Combine("CustomGroup", ComCode, $"{offseted:yyyyMM}.json");
+
+            var cache = JsonCache.Load<ETFGroup>(jsonFilePath);
+            if (cache != null)
+                return cache;
+
+            cache = new ETFGroup()
+            {
+                Name = $"{ComCode} 成份股",
+            };
+
+            var url = $"https://www.cathaysite.com.tw/ETF/detail/{SiteCode}?tab=etf3";
+
+            string content;
+            using (var page = ChromiumBrowser.NewPage(url))
+            {
+                page.WaitForSelectorAsync("div[data-anchor=\"股票\"] div.bar_table_body div.bar_table_line:nth-child(1)").Wait();
+                content = page.EvaluateFunctionAsync<string>(@"() => {
+return document.querySelector('div[data-anchor=""股票""] div.bar_table_body').innerHTML;
+}").Result;
+            }
+
+            IDocument doc = BrowsingContext.New(Configuration.Default.WithDefaultLoader())
+                .OpenAsync(req => req.Content(content)).Result;
+
+            var tds = doc.QuerySelectorAll("span.code:nth-child(1)");
+            foreach (var td in tds)
+                cache.ComCodes.Add(td.Text().Trim());
+
+            if (cache.ComCodes.Count > 0)
+                JsonCache.Store(jsonFilePath, cache);
+            return cache;
+        }
+    }
+}
--- StockApp/ETF/ETF00878.cs
+++ StockApp/ETF/ETF00878.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace StockApp.ETF
 {
-    class ETF00878 : YuanTaETFBase
+    class ETF00878 : CathayETFBase
     {
         public override string ComCode => "00878";
+
+        protected override string SiteCode => "ECN";
     }
 }
