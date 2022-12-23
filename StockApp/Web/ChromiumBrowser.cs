8f248619474116c8bcc3614a8840907e3c5ee7d3
phxswjjj <phxswjjj@gmail.com>
2022-12-23 10:17:20 +0800

    record request time elapsed

--- StockApp/Web/ChromiumBrowser.cs
+++ StockApp/Web/ChromiumBrowser.cs
@@ -1,6 +1,8 @@
 ﻿using PuppeteerSharp;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,20 +13,39 @@ namespace StockApp.Web
     {
         public static Lazy<IBrowser> Instance = new Lazy<IBrowser>(() =>
         {
+            var logger = Utility.LogHelper.Log;
+
+            var sw = new Stopwatch();
+            sw.Start();
             using (var browserFetcher = new BrowserFetcher())
             {
                 browserFetcher.DownloadAsync().Wait();
             }
             var browser = Puppeteer.LaunchAsync(new LaunchOptions { Headless = true }).Result;
+            sw.Stop();
+            logger
+                .ForContext("Elapsed", sw.ElapsedMilliseconds)
+                .Information("Browser Instance Initialized {Elapsed:#,##0} ms");
             return browser;
         });
 
         public static IPage NewPage(string url)
         {
+            var logger = Utility.LogHelper.Log;
+
             var browser = Instance.Value;
             var page = browser.NewPageAsync().Result;
             page.SetUserAgentAsync("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36").Wait();
+
+            var sw = new Stopwatch();
+            sw.Start();
             page.GoToAsync(url, WaitUntilNavigation.DOMContentLoaded).Wait();
+            sw.Stop();
+            logger
+                .ForContext("RequestUrl", url)
+                .ForContext("Elapsed", sw.ElapsedMilliseconds)
+                .Information("Load Page {Elapsed:#,##0} ms: {RequestUrl}");
+
             return page;
         }
     }
